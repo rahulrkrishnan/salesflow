66ad8930f90b831b18f702e858978596fa612f7e
Rahul <rahulrkrishnan@gmail.com>
2022-04-25 23:06:40 +0400

    v1 test 1

--- core.py
+++ core.py
@@ -1,1 +1,24 @@
-print ("corre hello")
+print ("Initialized")
+
+cname=input("Company Nname: ")
+cloc=input("Country: ")
+pbu=input("Which Business Unit: ")
+oval=input("Order Value: $")
+industry=input("Industry: ")
+presentation=input("Capability Presentation (y/n): " )
+
+#whats the timeline based on past experience
+#what other tools are being evaluated
+#whats the criteria for evaluating tools
+#did we have a discovery call to understand customer requirement
+#did we deliver a technical presentation
+#was this a lead?
+#was this opportunity developed by the SE
+#is this an Ansys lead
+#is this an FC lead
+#is the customer technically convinced of the product capabilities
+#which tool is the main competiton
+#did we submit a quote
+#is it a lease or perpetual
+#does other customers in same industry spend similar amount?
+#are there an other customers having similar business?
